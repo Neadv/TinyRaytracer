233b68e19306d618d9dd39e5d13f8c495eea2315
Vlad Krinitskiy <vlkrinitskiy@gmail.com>
2020-04-05 21:11:22 +0300

    Sphere and raytracing added

--- TinyRaytracer/Program.cs
+++ TinyRaytracer/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TinyRaytracer
 {
@@ -10,18 +6,34 @@ namespace TinyRaytracer
     {
         static void Main()
         {
-            FrameBuffer frameBuffer = new FrameBuffer(1024, 768);
+            int width = 1024;
+            int height = 728;
 
-            for (int y = 0; y < 768; y++)
+            FrameBuffer frameBuffer = new FrameBuffer(width, height);
+
+            Sphere sphere = new Sphere(new Vector3(-3, 0, -16), 4);
+            float fov = (float)Math.PI/2.0f;
+
+            for (int y = 0; y < height; y++)
             {
-                for (int x = 0; x < 1024; x++)
+                for (int x = 0; x < width; x++)
                 {
-                    frameBuffer.SetPixel(x, y, new Color(y / 768.0f, x / 1024.0f, 0));
+                    float xDir = (float)((2 * (x + 0.5) / (float)width - 1) * Math.Tan(fov / 2.0f) * width / (float)height);
+                    float yDir = -(float)((2 * (y + 0.5) / (float)height - 1) * Math.Tan(fov / 2.0f));
+                    Vector3 dir = new Vector3(xDir, yDir, -1).Normalize();
+                    frameBuffer.SetPixel(x, y, CastRay(new Vector3(), dir, sphere));
                 }
             }
             
             var image = frameBuffer.GetBitmap();
             image.Save("Render.png");
+        }
+        static Color CastRay(Vector3 orig, Vector3 dir, Sphere sphere)
+        {
+            float sphereDist = 0;
+            if (!sphere.RayIntersect(orig, dir, ref sphereDist))
+                return new Color(0.2f, 0.7f, 0.8f);
+            return new Color(0.4f, 0.4f, 0.3f);
         }
     }
 }
---
+++ TinyRaytracer/Sphere.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyRaytracer
+{
+    class Sphere
+    {
+        private Vector3 _center;
+        private float _radius;
+
+        public Sphere(Vector3 center, float radius)
+        {
+            _center = center;
+            _radius = radius;
+        }
+
+        public bool RayIntersect(Vector3 orig, Vector3 dir, ref float dist)
+        {
+            Vector3 pc = _center - orig;
+            Vector3 pcA = dir * dir.Dot(pc);
+            if ((pcA - _center).Length() > _radius)
+                return false;
+            float c = (float)Math.Sqrt(_radius * _radius - Math.Pow((_center - pcA).Length(), 2));
+            dist = pcA.Length() - c;
+            if (pc.Length() < _radius)
+                dist = pcA.Length() + c;
+            if (pc.Dot(dir) > 0)
+                dist = c - pcA.Length();
+            return true;
+        }
+    }
+}
